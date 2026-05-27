2523146641be00b96f71dde25c33ef7a8353cc91
Pratik Wani <pwani8306@gmail.com>
2026-05-27 14:39:13 +0530

    feat: Added apiEndpoints configuration file for managing backend route constants

---
+++ server/src/config/apiEndpoints.js
@@ -0,0 +1,12 @@
+const apiEndpoints = {
+  WEATHER_API:
+    "https://api.open-meteo.com/v1/forecast",
+
+  SENTINEL_API:
+    "https://sentinel-hub.com/api",
+
+  NASA_API:
+    "https://api.nasa.gov"
+};
+
+export default apiEndpoints;
